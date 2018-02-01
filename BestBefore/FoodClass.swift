ba6b910910ff47961fb8e4518ff05fa8f669b100
CAUADC <cauadc@CAUADC-29.local>
2018-02-01 12:26:41 +0900

    유통기한 알리미 첫 커밋입니다.

---
+++ BestBefore/FoodClass.swift
@@ -0,0 +1,9 @@
+//
+//  FoodClass.swift
+//  BestBefore
+//
+//  Created by CAUADC on 2018. 2. 1..
+//  Copyright © 2018년 0. All rights reserved.
+//
+
+import Foundation
---
+++ BestBefore/FoodDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FoodDetailViewController.swift
+//  BestBefore
+//
+//  Created by CAUADC on 2018. 2. 1..
+//  Copyright © 2018년 0. All rights reserved.
+//
+
+import UIKit
+
+class FoodDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
