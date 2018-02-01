e20e9b507de6fd74d8a6991a4536bccc2e224544
CAUADC <cauadc@CAUADC-29.local>
2018-02-01 12:26:55 +0900

    유통기한 알리미 첫 커밋입니다.

--- BestBefore/D-dayTableViewController.swift
+++ BestBefore/D-dayTableViewController.swift
@@ -9,6 +9,12 @@
 import UIKit
 
 class D_dayTableViewController: UITableViewController {
+    
+    
+    var foods:[Food] = []
+    
+   
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -18,6 +24,15 @@ class D_dayTableViewController: UITableViewController {
 
         // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
         // self.navigationItem.rightBarButtonItem = self.editButtonItem
+        
+        var food1 = Food(date: "2018. 01. 02", dDay: "D+7", foodImage: #imageLiteral(resourceName: "food1"), foodColor: UIColor.red)
+//        var food2 = Food(date: "2018. 01. 08", dDay: "D-1", foodImage: <#T##UIImage#>, foodColor: UIColor.yellow)
+//        var food3 = Food(date: "2018. 01. 15", dDay: "D-10", foodImage: <#T##UIImage#>, foodColor: UIColor.green)
+        
+        foods.append(food1)
+//        foods.append(food2)
+//        foods.append(food3)
+//
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,23 +44,29 @@ class D_dayTableViewController: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 3
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         // #warning Incomplete implementation, return the number of rows
-        return 0
+        return foods.count
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "food", for: indexPath)
 
-        // Configure the cell...
-
+        
+        var food = foods[indexPath.row]
+        
+        cell.imageView?.image = food.foodImage
+        cell.textLabel?.text = food.date
+        cell.detailTextLabel?.text = food.dDay
+        cell.backgroundColor = food.foodColor
+        
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
--- BestBefore/FoodClass.swift
+++ BestBefore/FoodClass.swift
@@ -7,3 +7,75 @@
 //
 
 import Foundation
+import UIKit
+
+class Settings {
+    var alarmTimes: Int
+    var alarmTypeBool: Bool
+    var alarmDate: Int
+    
+    func alarmTime() {
+        //alarmTimes만큼 알람 시간 설정
+    }
+    func alarmType() {
+        if alarmTypeBool == true {
+            //소리로 알람
+        } else {
+            //진동으로 알람
+        }
+    }
+    
+    init(alarmTimes:Int, alarmTypeBool: Bool, alarmDate: Int) {
+        self.alarmTimes = alarmTimes
+        self.alarmTypeBool = alarmTypeBool
+        self.alarmDate = alarmDate
+    }
+}
+
+class Food {
+    var date:String
+    var dDay:String
+    var foodImage:UIImage
+    var foodColor:UIColor
+    
+    init (date:String, dDay:String, foodImage:UIImage, foodColor:UIColor){
+        self.date = date
+        self.dDay = dDay
+        self.foodImage = foodImage
+        self.foodColor = foodColor
+    }
+    
+}
+
+//
+//class AutoItem : Settings {
+//    var bestBeforeDate:String
+//    var autoImage:UIImage
+//
+//    func dday() {
+//        //bestBeforeDate에서 현재 날짜 빼기
+//        //dday가 얼마일때 어떤 색깔로 view 표시
+//    }
+//
+//    init(alarmTimes:Int, alarmTypeBool: Bool, alarmDate: Int, bestBeforeDate:String, autoImage:UIImage) {
+//        self.bestBeforeDate = bestBeforeDate
+//        self.autoImage = autoImage
+//        super.init(alarmTimes: alarmTimes, alarmTypeBool: alarmTypeBool, alarmDate: alarmDate)
+//    }
+//}
+//
+//class ManualItem : Settings {
+//    var date:String
+//    var manualImage:UIImage
+//
+//    func ddayCalculate() {
+//        // date에서 앱 내에 저장된 값으로 디데이 계산
+//    }
+//
+//    init(alarmTimes:Int, alarmTypeBool: Bool, alarmDate: Int, date:String, manualImage:UIImage){
+//        self.date = date
+//        self.manualImage = manualImage
+//        super.init(alarmTimes: alarmTimes, alarmTypeBool: alarmTypeBool, alarmDate: alarmDate)
+//    }
+//}
+
