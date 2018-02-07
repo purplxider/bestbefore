ff9b0c3b7d59b602d578529709240e124618f9e3
1234sssssdddd <sue1704@naver.com>
2018-02-07 17:14:28 +0900

    소리 추가, 진동 버튼 색깔, 알림 리스트 스토리보드
    ㅇ_ㅇ

--- bestbefore/BestBefore/AlarmTableViewController.swift
+++ bestbefore/BestBefore/AlarmTableViewController.swift
@@ -11,14 +11,33 @@ import AVFoundation
 import AudioToolbox
 
 class AlarmTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
-
-    @IBAction func Vibrate(_ sender: Any) {
-        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+    
+    func UIColorFromRGB(rgbValue: UInt) -> UIColor {
+        return UIColor(
+            red: CGFloat((rgbValue & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgbValue & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgbValue & 0x0000FF) / 255.0,
+            alpha: CGFloat(1.0)
+        )
     }
     
-    @IBAction func Sound(_ sender: Any) {
-        AudioServicesPlaySystemSound(SystemSoundID( ))
-    }
+    @IBAction func Vibrate(_ sender: Any){
+        AudioServicesPlaySystemSound(SystemSoundID(kSystemSoundID_Vibrate))
+        
+    } // 진동
+    
+    @IBAction func Sound(_ sender: Any) { AudioServicesPlaySystemSound(SystemSoundID(1005))
+    } // 소리
+    
+    @IBOutlet weak var Vibrate: UIButton!
+    @IBAction func colorChangeVibrate(_ sender: Any) {
+        UIView.animate(withDuration: 1, animations: { self.Vibrate.backgroundColor = self.UIColorFromRGB(rgbValue: 0xFFD1D1) })
+    } // 진동 색깔
+    
+    @IBOutlet weak var Sound: UIButton!
+    @IBAction func colorChangeSound(_ sender: Any) {
+         UIView.animate(withDuration: 1, animations: { self.Vibrate.backgroundColor = self.UIColorFromRGB(rgbValue: 0xFFD1D1) })
+    } //소리 색깔
     
     @IBOutlet weak var datePickerTxt: UITextField! // 시간 설정
     @IBOutlet weak var dayPickerTxt: UITextField! // 날짜 설정
@@ -74,74 +93,5 @@ class AlarmTableViewController: UITableViewController, UIPickerViewDelegate, UIP
         dayPickerTxt.text = counts[row]
         dayPickerTxt.resignFirstResponder()
     }
-
-    
-
-    // MARK: - Table view data source
-
-//    override func numberOfSections(in tableView: UITableView) -> Int {
-//        // #warning Incomplete implementation, return the number of sections
-//        return 0
-//    }
-//
-//    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-//        // #warning Incomplete implementation, return the number of rows
-//        return 0
-//    }
-
-    /*
-    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
-
-        // Configure the cell...
-
-        return cell
-    }
-    */
-
-    /*
-    // Override to support conditional editing of the table view.
-    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the specified item to be editable.
-        return true
-    }
-    */
-
-    /*
-    // Override to support editing the table view.
-    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
-        if editingStyle == .delete {
-            // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
-        } else if editingStyle == .insert {
-            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
-        }    
-    }
-    */
-
-    /*
-    // Override to support rearranging the table view.
-    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
-
-    }
-    */
-
-    /*
-    // Override to support conditional rearranging of the table view.
-    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
-        // Return false if you do not want the item to be re-orderable.
-        return true
-    }
-    */
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
