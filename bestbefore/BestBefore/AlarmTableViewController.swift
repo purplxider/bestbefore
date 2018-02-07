292af9dc5cf368235af47c00410c4858c9afc3ca
1234sssssdddd <sue1704@naver.com>
2018-02-07 15:19:36 +0900

    알람에 시간 넣음
    ..

--- bestbefore/BestBefore/AlarmTableViewController.swift
+++ bestbefore/BestBefore/AlarmTableViewController.swift
@@ -8,19 +8,27 @@
 
 import UIKit
 
-class AlarmTableViewController: UITableViewController {
+class AlarmTableViewController: UITableViewController, UIPickerViewDelegate, UIPickerViewDataSource {
 
-    // 시간 설정
-    @IBOutlet weak var datePickerTxt: UITextField!
+    @IBOutlet weak var datePickerTxt: UITextField! // 시간 설정
+    @IBOutlet weak var dayPickerTxt: UITextField! // 날짜 설정
     
     let datePicker = UIDatePicker()
+    let counts = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+    var countPicker = UIPickerView()
+    var dayPicker = UIPickerView()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
         createDatePicker()
-    }
     
+        dayPicker.delegate = self
+        dayPicker.dataSource = self
+        dayPickerTxt.inputView = dayPicker
+        
+    }
+    // 시간 설정
     func createDatePicker() {
         datePicker.datePickerMode = .time
         
@@ -29,9 +37,7 @@ class AlarmTableViewController: UITableViewController {
         
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: nil, action: #selector(donePressed))
         toolbar.setItems([doneButton], animated: false)
-        
         datePickerTxt.inputAccessoryView = toolbar
-        
         datePickerTxt.inputView = datePicker
     }
     
@@ -44,9 +50,26 @@ class AlarmTableViewController: UITableViewController {
     }
     
     
+    //날짜 설정
+    public func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 1
+    }
+    public func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+        return counts.count
+    }
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        return counts[row]
+    }
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        dayPickerTxt.text = counts[row]
+        dayPickerTxt.resignFirstResponder()
+    }
 
-
-
+    
+    
+    
+    
+    
     // MARK: - Table view data source
 
 //    override func numberOfSections(in tableView: UITableView) -> Int {
