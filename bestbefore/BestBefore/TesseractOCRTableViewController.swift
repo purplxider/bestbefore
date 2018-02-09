2d8b5e9af42cd999d3ad9b47d7eb50b1cf027e72
1234sssssdddd <sue1704@naver.com>
2018-02-09 19:20:30 +0900

    자동추가 테이블뷰로 바꿈
    ㅇㅇ

--- bestbefore/BestBefore/TesseractOCRTableViewController.swift
+++ bestbefore/BestBefore/TesseractOCRTableViewController.swift
@@ -1,15 +1,15 @@
 //
-//  TesseractOCRViewController.swift
+//  TesseractOCRTableViewController.swift
 //  BestBefore
 //
-//  Created by CAUADC on 2018. 2. 8..
+//  Created by CAUADC on 2018. 2. 9..
 //  Copyright © 2018년 0. All rights reserved.
 //
 
 import UIKit
 import TesseractOCR
 
-class TesseractOCRViewController: UIViewController {
+class TesseractOCRTableViewController: UITableViewController {
     
     @IBOutlet weak var textView: UITextView!
     
@@ -62,11 +62,11 @@ class TesseractOCRViewController: UIViewController {
 
 
 // UINavigationControllerDelegate
-extension TesseractOCRViewController: UINavigationControllerDelegate {
+extension TesseractOCRTableViewController : UINavigationControllerDelegate {
 }
 
 // UIImagePickerControllerDelegate
-extension TesseractOCRViewController: UIImagePickerControllerDelegate {
+extension TesseractOCRTableViewController : UIImagePickerControllerDelegate {
     func presentImagePicker() {
         
         let imagePickerActionSheet = UIAlertController(title: "어디서 사진을 가져올까",
@@ -100,10 +100,10 @@ extension TesseractOCRViewController: UIImagePickerControllerDelegate {
     
     func imagePickerController(_ picker: UIImagePickerController,
                                didFinishPickingMediaWithInfo info: [String : Any]) {
-        
+
         if let selectedPhoto = info[UIImagePickerControllerOriginalImage] as? UIImage,
             let scaledImage = selectedPhoto.scaleImage(640) {
-            
+
             dismiss(animated: true, completion: {
                 self.performImageRecognition(scaledImage)
             })
@@ -114,9 +114,9 @@ extension TesseractOCRViewController: UIImagePickerControllerDelegate {
 // MARK: - UIImage extension
 extension UIImage {
     func scaleImage(_ maxDimension: CGFloat) -> UIImage? {
-        
+
         var scaledSize = CGSize(width: maxDimension, height: maxDimension)
-        
+
         if size.width > size.height {
             let scaleFactor = size.height / size.width
             scaledSize.height = scaledSize.width * scaleFactor
@@ -124,93 +124,13 @@ extension UIImage {
             let scaleFactor = size.width / size.height
             scaledSize.width = scaledSize.height * scaleFactor
         }
-        
+
         UIGraphicsBeginImageContext(scaledSize)
         draw(in: CGRect(origin: .zero, size: scaledSize))
         let scaledImage = UIGraphicsGetImageFromCurrentImageContext()
         UIGraphicsEndImageContext()
-        
+
         return scaledImage
     }
 }
-
-
-//    @IBOutlt weak var textView: UITextView!
-//
-//    let picker = UIImagePickerController()
-//
-//    func openLibrary() {
-//        picker.sourceType = .photoLibrary
-//        present(picker, animated: false, completion: nil)
-//    }
-//
-//    func openCamera() {
-//        picker.sourceType = .camera
-//        present(picker, animated: false, completion: nil)
-//    }
-//
-//    @IBAction func addAnImage(_ sender: Any) {
-//        let alert = UIAlertController(title: "어디서 사진을 가져올까", message: "골라줘", preferredStyle: .actionSheet)
-//
-//        let library = UIAlertAction(title: "사진앨범", style: .default) { (action) in self.openLibrary()
-//        }
-//
-//        let camera = UIAlertAction(title: "카메라", style: .default) { (action) in
-//            self.openCamera()
-//        }
-//
-//        let cancel = UIAlertAction(title: "취소", style: .cancel, handler: nil)
-//
-//        alert.addAction(library)
-//        alert.addAction(camera)
-//        alert.addAction(cancel)
-//
-//        present(alert, animated: true, completion: nil)
-//    }
-//
-//    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
-//        if let image = info[UIImagePickerControllerOriginalImage] as? UIImage{
-//            addImage.image = image
-//            print(info)
-//        }
-//        dismiss(animated: true, completion: nil)
-//    }
-//
-//
-//
-//
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//
-//        picker.delegate = self
-//
-//        if let tesseract = G8Tesseract(language:"eng") {
-//            tesseract.delegate = self
-//            tesseract.image = UIImage(named: "testImage1")?.g8_blackAndWhite()
-//            tesseract.recognize()
-//
-//            textView.text = tesseract.recognizedText
-//        // Uncomment the following line to preserve selection between presentations
-//        // self.clearsSelectionOnViewWillAppear = false
-//
-//        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-//        // self.navigationItem.rightBarButtonItem = self.editButtonItem
-//    }
-//
-//
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//
-//    func progressImageRecognition(for tesseract : G8Tesseract!) {
-//        print("Recognition Progress\(tesseract.progress) %")
-//    }
-//
-//    override func didReceiveMemoryWarning() {
-//        super.didReceiveMemoryWarning()
-//        // Dispose of any resources that can be recreated.
-//    }
-//}
 
