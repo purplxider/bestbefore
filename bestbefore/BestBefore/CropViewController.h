5749a9fe428f77475b53ddfc41be165460697a95
hyobbu <hkryu0719@gmail.com>
2018-05-24 19:15:25 +0900

    크롭기능, 크롭된사진 ocr인식 추가

---
+++ bestbefore/BestBefore/CropViewController.h
@@ -0,0 +1,18 @@
+//
+//  CropViewController.h
+//  BestBefore
+//
+//  Created by MBP05 on 2018. 5. 24..
+//  Copyright © 2018년 0. All rights reserved.
+//
+
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for CropViewController.
+FOUNDATION_EXPORT double CropViewControllerVersionNumber;
+
+//! Project version string for CropViewController.
+FOUNDATION_EXPORT const unsigned char CropViewControllerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CropViewController/PublicHeader.h>
